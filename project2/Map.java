e9686771a47d62f35efc36b387164e2141869ee4
mmasenheimer <mmasenheimer21@gmail.com>
2025-10-23 23:51:47 -0700

    changed line spacing

--- project2/Map.java
+++ project2/Map.java
@@ -108,6 +108,7 @@ public class Map {
         this.OCCUPIED = this.NUMTERRITORIES - this.VICTIMS;
         this.MAXDICE = maxDice;
         this.players = players;
+        
 
         this.theMap = new Territory[rows][columns];
 
